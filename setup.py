e30d2ca6384e874e0f135f748cdf7293e8523488
Henri Kuiper <henri@Henris-MacBook-Pro.local>
2013-03-05 22:26:02 +0100

    initial version for pip

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = "mlocs",
+    packages = ["mlocs"],
+    version = "1.0.0",
+    description = "Effective Location Storage via a Maidenhead Locator System For Python",
+    author = "Henri Kuiper",
+    author_email = "henrikuiper@zdevops.com",
+    url = "http://zdevops.com",
+    download_url = "",
+    keywords = ["location", "maidenhead", "effective"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        ],
+)
