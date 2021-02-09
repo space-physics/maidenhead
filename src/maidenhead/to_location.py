704635044ceaa800467ccf827e98e88d44933c0a
Hilary Jendrasiak <sylogista@sylogista.pl>
2021-02-09 23:49:43 +0100

    added option: --center
    to_location default return is unchanged, i.e. lat lon of south-west corner of the provided maidenhead grid square. --center option is fixing to_location return to the center of that grid square; Module after modification is completly backward compatible.

--- src/maidenhead/to_location.py
+++ src/maidenhead/to_location.py
@@ -1,7 +1,7 @@
 import typing as T
 
 
-def to_location(maiden: str) -> T.Tuple[float, float]:
+def to_location(maiden: str, center: bool = False) -> T.Tuple[float, float]:
     """
     convert Maidenhead grid to latitude, longitude
 
@@ -10,6 +10,10 @@ def to_location(maiden: str) -> T.Tuple[float, float]:
 
     maiden : str
         Maidenhead grid locator of length 2 to 8
+
+    center : bool
+        If true, return the center of provided maidenhead grid square, instead of default south-west corner
+        Default value = False needed to maidenhead full backward compatibility of this module.
 
     Returns
     -------
@@ -42,5 +46,20 @@ def to_location(maiden: str) -> T.Tuple[float, float]:
     if N >= 8:
         lon += int(maiden[6]) * 5.0 / 600
         lat += int(maiden[7]) * 2.5 / 600
+
+	# %% move lat lon to the center (if requested)
+    if center:
+        if N == 2:
+            lon += 20 / 2
+            lat += 10 / 2
+        elif N == 4:
+            lon += 2 / 2
+            lat += 1.0 / 2
+        elif N == 6:
+            lon += 5.0 / 60 / 2
+            lat += 2.5 / 60 / 2
+        elif N >= 8:
+            lon += 5.0 / 600 / 2
+            lat += 2.5 / 600 / 2
 
     return lat, lon
